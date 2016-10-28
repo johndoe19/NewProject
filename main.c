b371cfe3fc8b4232009a07f4aac9707f4d36a6ec
admin <admin@example.com>
2016-10-28 18:45:40 +0300

    no comment

--- main.c
+++ main.c
@@ -2,23 +2,8 @@
 
 main()
 {
-	//double n = 5.f;
-	//double d = 3.1415926;
-	//double a, b;
-
 	double a;
 	int b = 7, c = 2;
 	a = (double)(b / c);
 	printf("a=%f\n", a);
-
-	//printf("Hello world!!\npi = %f\n", d);
-	//printf("pi = %f\npi*pi = %f\n", d, d*d);
-	/*
-	printf("%d %o %x\n", 100, 100, 100);
-	printf("%d %d %d\n", 100, 0x64, 0144);
-
-	printf("input a & b: ");
-	scanf_s("%lf & %lf", &a, &b);
-	printf("a = %f\tb=%f\n", a, b);
-	*/
 }
